4e79ae903e943c9cdbebfdc9e50a9aed4c8678f4
oywino <oyvindo@compuserve.com>
2025-05-30 17:15:01 +0200

    v0.2.5 revived to v0.3.1
    also corrected name to "xcomfort-python"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xcomfort-python",
-    version="0.2.5",
+    version="0.3.1",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     maintainer="oyvind overby",
--- xcomfort/devices.py
+++ xcomfort/devices.py
@@ -138,9 +138,11 @@ class Shade(BridgeDevice):
         self.payload = {}
 
     @property
-    def supports_go_to(self) -> bool:
-        raise Exception("Temporary exception: supports_go_to property called")
-        return True
+    def supports_go_to(self) -> bool | None:
+        """Check if the shade supports precise position control."""
+        if (component := self.bridge._comps.get(self.comp_id)) is not None:
+            return component.comp_type == 86 and self.__shade_state.raw.get("shRuntime") == 1
+        return None
 
     def handle_state(self, payload):
         """Update the shade state with incoming data."""
