e2a86f2faab380236f5dcc3716485d2ac4a2e853
oywino <oyvindo@compuserve.com>
2025-05-30 18:00:29 +0200

    Exception test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xcomfort-python",
-    version="0.3.3",
+    version="0.3.4",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     maintainer="oyvind overby",
