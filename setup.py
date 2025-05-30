06397cd7bc9a4a81ded409a78e83a1128555bd06
oywino <oyvindo@compuserve.com>
2025-05-30 14:35:32 +0200

    Still no set_position, trying again
    bump to v0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xcomfort",
-    version="0.2.6",
+    version="0.2.7",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     maintainer="oyvind overby",
