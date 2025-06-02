5c76e9d1703a81364549df3cf3df0bd96e1bc70a
oywino <oyvindo@compuserve.com>
2025-06-02 22:58:08 +0200

    v0.7.3 was a mistake. Redone

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xcomfort",
-    version="0.7.3",
+    version="0.7.4",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     maintainer="oyvind overby",
