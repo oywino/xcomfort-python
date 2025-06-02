2075501feef2329d7878c01408273ac15284736d
oywino <oyvindo@compuserve.com>
2025-06-02 13:06:03 +0200

    Updated setup.py to v0.2.5
    This is true copy of v0.6.1 from the experiment branch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="xcomfort",
-    version="0.6.1",
+    version="0.2.5",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     maintainer="oyvind overby",
